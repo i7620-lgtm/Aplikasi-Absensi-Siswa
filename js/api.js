50e30a2c830ae5e6c0d82df389cc2467ee5cc116
i7620-lgtm <i7620@guru.sd.belajar.id>
2025-09-24 15:11:19 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -32,10 +32,6 @@ export const apiService = {
         return await _fetch('saveData', data);
     },
 
-    async importData(data) {
-        return await _fetch('importData', data);
-    },
-
     async getDashboardData() {
         return await _fetch('getDashboardData');
     },
