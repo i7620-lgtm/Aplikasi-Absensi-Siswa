89e12f048e74662d11a331d27fe826d619e8c240
i7620-lgtm <i7620@guru.sd.belajar.id>
2025-10-07 18:09:35 +0800

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,4 @@
+
 import { state, setState, navigateTo, handleStartAttendance, handleManageStudents, handleViewHistory, handleDownloadData, handleSaveNewStudents, handleExcelImport, handleDownloadTemplate, handleSaveAttendance, handleGenerateAiRecommendation, handleCreateSchool, CLASSES, handleViewRecap, handleDownloadFullSchoolReport, handleMigrateLegacyData, handleDownloadJurisdictionReport } from './main.js';
 import { templates, getRoleDisplayName, encodeHTML } from './templates.js';
 import { handleSignOut, renderSignInButton } from './auth.js';
